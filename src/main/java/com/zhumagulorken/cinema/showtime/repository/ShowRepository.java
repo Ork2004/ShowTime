31e0b0a980c97cf9806efb3d1b4012baa7d1d5e6
Ork2004 <120731187+Ork2004@users.noreply.github.com>
2025-09-18 23:51:14 +0500

    Implement nested REST API relations (Movie → Show)

--- src/main/java/com/zhumagulorken/cinema/showtime/repository/ShowRepository.java
+++ src/main/java/com/zhumagulorken/cinema/showtime/repository/ShowRepository.java
@@ -4,6 +4,11 @@ import com.zhumagulorken.cinema.showtime.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ShowRepository extends JpaRepository<Show,Long> {
+    List<Show> findByMovieId(Long movieId);
+    Optional<Show> findByIdAndMovieId(Long id, Long movieId);
 }
