ef3922abb1b38fb2618fa742ac66ad37dc198d3a
Ork2004 <120731187+Ork2004@users.noreply.github.com>
2025-09-24 23:59:30 +0500

    feat: add validation layer with SeatDto and exception handler

---
+++ src/main/java/com/zhumagulorken/cinema/showtime/dto/SeatDto.java
@@ -0,0 +1,49 @@
+package com.zhumagulorken.cinema.showtime.dto;
+
+import jakarta.validation.constraints.Min;
+
+public class SeatDto {
+    private Long id;
+
+    @Min(value = 1, message = "Row number must be at least 1")
+    private int rowNumber;
+
+    @Min(value = 1, message = "Seat number must be at least 1")
+    private int seatNumber;
+
+    private Long hallId;
+
+    //Getter and Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Long getHallId() {
+        return hallId;
+    }
+
+    public void setHallId(Long hallId) {
+        this.hallId = hallId;
+    }
+}
