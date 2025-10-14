ec71476addee24053a411ad274161a1a5d15f0a3
Ork2004 <120731187+Ork2004@users.noreply.github.com>
2025-10-14 18:12:24 +0500

    feat: add About and Auth pages

--- src/main/java/com/zhumagulorken/cinema/showtime/config/SecurityConfig.java
+++ src/main/java/com/zhumagulorken/cinema/showtime/config/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/index.html",
+                                "/about.html",
+                                "/auth.html",
                                 "/css/**",
                                 "/js/**",
                                 "/swagger-ui/**",
